110f3d32daeba16b71eb3ab8fe59bad55a5e887d
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-29 23:15:07 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+      let result=record.find(o=>o.result==="W");
+      if(Boolean(result)===true)
+      return result.year;
+
+      //return undefined;
+
+}
