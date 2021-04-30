12d018f3fd38aa9cbdced6fa2b2c2b3b8a844db5
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-30 21:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ function testFunc() {
   return "hi"
 }
 function superbowlWin(record){
+<<<<<<< HEAD
       let result=record.find(o=>o.result==="W");
       if(Boolean(result)===true)
       return result.year;
 
       //return undefined;
 
+=======
+  return record.find(
+    record.result=>record.result==="W")
+    return record.year;
+  );
+>>>>>>> b5c06ad5522715a496d88a386acf89caf358a804
 }
